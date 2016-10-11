8c72bc0296753aedce05c1b43a64ba0fa9fd449a
Jeronimo Bezerra <jab.jero@gmail.com>
2016-10-11 02:25:37 -0400

    changing the messaging.

--- libs/cal/message.py
+++ libs/cal/message.py
@@ -2,129 +2,204 @@
     Messages sent/received through the MessageBroker
 """
 
+# Possible Payloads
+ID_NEGOTIATION = 0
+HELLO = 1
+REJECT = 2
+UPDATE = 3
+
+# Variables
+FIRST_ID = 1
+LAST_ID = 254
+CORE_ID = 255
+
 
 class Message:
 
-    def __init__(self):
-        self.header = self.Header()
-        self.body = self.Body()
+    def __init__(self, msg):
+        self.header = self.Header(msg['header'])
+        self.body = self.Body(self.header.payload, msg['body'])
 
-    def validate_message_format(self, msg):
-        if self.validate_header(msg['header']):
-            if self.validate_body(msg['body']):
-                if self.validate_protocol():
-                    return True
+    def validate_message_format(self):
+        if self.validate_header() and self.validate_body():
+            if self.validate_protocol():
+                return True
         return False
 
-    def validate_header(self, header):
-        return True if self.header.process_header(header) else False
+    def validate_header(self):
+        return True if (self.header.version is not None and self.header.id is not None
+                        and self.header.payload is not None
+                        and self.header.timing is not None
+                        and self.header.ipp is not None) else False
 
-    def validate_body(self, body):
-        return True if self.body.process_body(body) else False
+    def validate_body(self):
+        if self.body.header_payload != 3:
+            if self.body.suggested_id is not None:
+                return True
+        else:
+            if self.body.action is not None:
+                return True
+        return False
 
     def validate_protocol(self):
         return True if self.validate_all_fields() else False
 
     def validate_all_fields(self):
-        if self.header.id == 0:
-            # ID negotiation
-            if self.header.payload != 0:
-                return False
-        elif self.header.id in range(1, 254):
+        """
+            Returns:
+        """
+        if self.header.id == 0 and self.header.payload != 0:
+            # ID negotiation - ID and Payload must be 0
+            return False
+        elif self.header.id > 0 and self.header.payload not in [1, 3]:
             # Communication Established - Payload == 1(Hello) or 3(Update)
-            if self.header.payload not in [1, 3]:
-                return False
-        elif self.header.id == 255:
+            return False
+        elif self.header.id == 255 and self.header.payload not in [1, 2, 3]:
             # Core ID, can use Payload 1(Hello), 2(Reject) or 3(Update)
-            if self.header.payload not in [1, 2, 3]:
-                return False
+            return False
         return True
 
-    class Header:
+    class Header(object):
         """
         """
-        def __init__(self):
-            self.version = 1
-            self.id = 0
-            self.payload = 0
-            self.timing = 0
-            self.ipp = 0
+        def __init__(self, header):
+            self.__version = None
+            self.__id = None
+            self.__payload = None
+            self.__timing = None
+            self.__ipp = None
 
-        def _is_valid_version(self):
-            if self.version != 1:
-                raise ValueError
-            return True
+            self.__instantiate_vars(header)
 
-        def _is_valid_id(self):
-            if not (0 <= self.id <= 255):
-                raise ValueError
-            return True
+        def __instantiate_vars(self, header):
+            self.version = header['version']
+            self.id = header['id']
+            self.payload = header['payload']
+            self.timing = header['timing']
+            self.ipp = header['ipp']
 
-        def _is_valid_payload(self):
-            if not (0 <= self.payload <= 255):
-                raise ValueError
-            return True
+        @property
+        def version(self):
+            return self.__version
 
-        def _is_valid_timing(self):
-            if not (self.timing >= 0):
-                raise ValueError
-            return True
-
-        def _is_valid_ipp(self, ipp):
-
-            def check_port(port):
-                try:
-                    return 1 <= int(port) <= 65535
-                except (AttributeError, ValueError, TypeError):
-                    return False
-
-            def check_ip(ip):
-                try:
-                    parts = ip.split('.')
-                    return len(parts) == 4 and all(0 <= int(part) < 256 for part in parts)
-                except (AttributeError, TypeError, ValueError):
-                    return False
-
-            ip, port = ipp.split(':')
-            if check_ip(ip) and check_port(port):
-                self.ipp = ipp
-                return True
-
-            raise ValueError
-
-
-        def process_header(self, header):
-
+        @version.setter
+        def version(self, my_version):
             try:
-                self.version = int(header['version'])
-                self.id = int(header['id'])
-                self.payload = int(header['payload'])
-                self.timing = int(header['timing'])
-                if(
-                   (self._is_valid_version()) and
-                   (self._is_valid_id()) and
-                   (self._is_valid_payload()) and
-                   (self._is_valid_timing()) and
-                   (self._is_valid_ipp(header['ipp']))):
-                    return True
-
+                if int(my_version) == 1:
+                    # raise ValueError("Invalid Version: Only Version 1 is supported")
+                    self.__version = int(my_version)
             except ValueError:
-                return False
+                pass
 
-    class Body:
-        """
+        @property
+        def id(self):
+            return self.__id
 
-        """
-        def __init__(self):
-            self.suggested_id = None
-
-        def process_body(self, body):
+        @id.setter
+        def id(self, my_id):
             try:
-                if 0 < int(body['suggested_id']) <= 255:
-                    self.suggested_id = body['suggested_id']
-                    return True
-                else:
-                    raise ValueError
+                if 0 <= int(my_id) < 255:
+                    # raise ValueError(("ID must be between 0 and 255. Received: %s" % my_id))
+                    self.__id = int(my_id)
+            except ValueError:
+                pass
 
-            except (ValueError, TypeError):
-                return False
+        @property
+        def payload(self):
+            return self.__payload
+
+        @payload.setter
+        def payload(self, my_payload):
+            try:
+                if 0 <= int(my_payload) < 255:
+                    # raise ValueError("Payload must be between 0 and 255")
+                    self.__payload = int(my_payload)
+            except ValueError:
+                pass
+
+        @property
+        def timing(self):
+            return self.__timing
+
+        @timing.setter
+        def timing(self, my_timing):
+            try:
+                if int(my_timing) >= 0:
+                    # raise ValueError("Timing must be > 0")
+                    self.__timing = int(my_timing)
+            except ValueError:
+                pass
+
+        @property
+        def ipp(self):
+            return self.__ipp
+
+        @ipp.setter
+        def ipp(self, my_ipp):
+            try:
+                ip, port = my_ipp.split(':')
+                if 1 <= int(port) <= 65535:
+                    parts = ip.split('.')
+                    if len(parts) == 4 and all(0 <= int(part) < 256 for part in parts):
+                        self.__ipp = my_ipp
+            except (ValueError, AttributeError):
+                pass
+
+    class Body(object):
+        """
+
+        """
+        ACTIONS = ['switch_config', 'error', 'msg_received', 'entry_removed',
+                   'add_entry', 'get_statistics', 'send_probe']
+
+        def __init__(self, header_payload, body):
+            self.header_payload = header_payload
+            self.__suggested_id = None
+            self.__dpid = None
+            self.__action = None
+            self.__data = dict()
+
+            if self.header_payload != UPDATE:
+                self.suggested_id = body['suggested_id']
+            else:
+                self.dpid = body['dpid']
+                self.action = body['dpid']
+                self.data = body['dpid']
+
+        @property
+        def suggested_id(self):
+            return self.__suggested_id
+
+        @suggested_id.setter
+        def suggested_id(self, sid):
+            try:
+                if 1 <= int(sid) < 255:
+                    # raise ValueError(("ID must be between 0 and 255. Received: %s" % my_id))
+                    self.__suggested_id = int(sid)
+            except ValueError:
+                pass
+
+        @property
+        def dpid(self):
+            return self.__dpid
+
+        @dpid.setter
+        def dpid(self, did):
+            self.__dpid = did
+
+        @property
+        def action(self):
+            return self.__action
+
+        @action.setter
+        def action(self, act):
+            if self.action in self.ACTIONS:
+                self.__action = act
+
+        @property
+        def data(self):
+            return self.__data
+
+        @data.setter
+        def data(self, dt):
+            self.__data = dt
--- tests/test_message.py
+++ tests/test_message.py
@@ -1,115 +1,155 @@
 import unittest
-#from mock import Mock
+# from mock import Mock
 from libs.cal.message import Message
 from libs.cal.cal import CoreCal
 
 
-msg_recv = {
-                "header": {
-                    "version": 1,
-                    "id": 0,
-                    "payload": 1,
-                    "timing": 1,
-                    "ipp": "192.168.56.101:6633"
-                },
-                "body": {
-                    "suggested_id": 1
-                }
-           }
-
-
 class TestMessageHeader(unittest.TestCase):
-
-    def setUp(self):
-        self.my_msg_recv = msg_recv
-        self.message = Message()
 
     # Test message.header.version
     def test_wrong_versions(self):
+        msg_recv = {"header": {"version": 1, "id": 0, "payload": 1, "timing": 1, "ipp": "192.168.56.101:6633"}}
+        my_msg_recv = msg_recv
         values = [0, 'a', -1, ""]
         for value in values:
-            self.my_msg_recv['header']['version'] = value
-            self.assertEqual(self.message.validate_header(self.my_msg_recv['header']), False)
+            my_msg_recv['header']['version'] = value
+            self.message = Message(my_msg_recv)
+            self.assertEqual(self.message.validate_header(), False)
+            del self.message
 
-    def test_rigt_versions(self):
+    def test_right_versions(self):
+        msg_recv = {"header": {"version": 1, "id": 0, "payload": 1, "timing": 1, "ipp": "192.168.56.101:6633"}}
+        my_msg_recv = msg_recv
         values = [1, '1']
         for value in values:
-            self.my_msg_recv['header']['version'] = value
-            self.assertEqual(self.message.validate_header(self.my_msg_recv['header']), True)
+            my_msg_recv['header']['version'] = value
+            self.message = Message(my_msg_recv)
+            self.assertEqual(self.message.validate_header(), True)
+            del self.message
 
     # Test message.header.id
     def test_wrong_ids(self):
+        msg_recv = {"header": {"version": 1, "id": 0, "payload": 1, "timing": 1, "ipp": "192.168.56.101:6633"}}
+        my_msg_recv = msg_recv
         values = [256, 'a', -1, ""]
         for value in values:
-            self.my_msg_recv['header']['id'] = value
-            self.assertEqual(self.message.validate_header(self.my_msg_recv['header']), False)
+            my_msg_recv['header']['id'] = value
+            self.message = Message(my_msg_recv)
+            self.assertEqual(self.message.validate_header(), False)
+            del self.message
 
-    def test_rigt_ids(self):
+    def test_right_ids(self):
+        msg_recv = {"header": {"version": 1, "id": 0, "payload": 1, "timing": 1, "ipp": "192.168.56.101:6633"}}
+        my_msg_recv = msg_recv
         values = [0, 1, '1']
         for value in values:
-            self.my_msg_recv['header']['id'] = value
-            self.assertEqual(self.message.validate_header(self.my_msg_recv['header']), True)
+            my_msg_recv['header']['id'] = value
+            self.message = Message(my_msg_recv)
+            self.assertEqual(self.message.validate_header(), True)
+            del self.message
 
     # Test message.header.payload
     def test_wrong_payloads(self):
+        msg_recv = {"header": {"version": 1, "id": 0, "payload": 1, "timing": 1, "ipp": "192.168.56.101:6633"}}
+        my_msg_recv = msg_recv
         values = [256, 'a', -1, ""]
         for value in values:
-            self.my_msg_recv['header']['payload'] = value
-            self.assertEqual(self.message.validate_header(self.my_msg_recv['header']), False)
+            my_msg_recv['header']['payload'] = value
+            self.message = Message(my_msg_recv)
+            self.assertEqual(self.message.validate_header(), False)
+            del self.message
 
-    def test_rigt_payloads(self):
+    def test_right_payloads(self):
+        msg_recv = {"header": {"version": 1, "id": 0, "payload": 1, "timing": 1, "ipp": "192.168.56.101:6633"}}
+        my_msg_recv = msg_recv
         values = [0, 1, '1']
         for value in values:
-            self.my_msg_recv['header']['payload'] = value
-            self.assertEqual(self.message.validate_header(self.my_msg_recv['header']), True)
+            my_msg_recv['header']['payload'] = value
+            self.message = Message(my_msg_recv)
+            self.assertEqual(self.message.validate_header(), True)
+            del self.message
 
     # Test message.header.timing
     def test_wrong_timings(self):
-        values = [256, 'a', -1, ""]
+        msg_recv = {"header": {"version": 1, "id": 0, "payload": 1, "timing": 1, "ipp": "192.168.56.101:6633"}}
+        my_msg_recv = msg_recv
+        values = ['a', -1, ""]
         for value in values:
-            self.my_msg_recv['header']['timing'] = value
-            self.assertEqual(self.message.validate_header(self.my_msg_recv['header']), False)
+            my_msg_recv['header']['timing'] = value
+            self.message = Message(my_msg_recv)
+            self.assertEqual(self.message.validate_header(), False)
+            del self.message
 
-    def test_rigt_timings(self):
+    def test_right_timings(self):
+        msg_recv = {"header": {"version": 1, "id": 0, "payload": 1, "timing": 1, "ipp": "192.168.56.101:6633"}}
+        my_msg_recv = msg_recv
         values = [0, 1, '1']
         for value in values:
-            self.my_msg_recv['header']['timing'] = value
-            self.assertEqual(self.message.validate_header(self.my_msg_recv['header']), True)
+            my_msg_recv['header']['timing'] = value
+            self.message = Message(my_msg_recv)
+            self.assertEqual(self.message.validate_header(), True)
+            del self.message
 
     # Test message.header.ipp
     def test_wrong_ipps(self):
+        msg_recv = {"header": {"version": 1, "id": 0, "payload": 1, "timing": 1, "ipp": "192.168.56.101:6633"}}
+        my_msg_recv = msg_recv
         values = [256, 'a', -1, "", "0.0.1.0:100000", "12.4.2:2222", "255.255.255:65535", "12.4.2.22"]
         for value in values:
-            self.my_msg_recv['header']['ipp'] = value
-            self.assertEqual(self.message.validate_header(self.my_msg_recv['header']), False)
+            my_msg_recv['header']['ipp'] = value
+            self.message = Message(my_msg_recv)
+            self.assertEqual(self.message.validate_header(), False)
+            del self.message
 
-    def test_rigt_ipp(self):
+    def test_right_ipp(self):
+        msg_recv = {"header": {"version": 1, "id": 0, "payload": 1, "timing": 1, "ipp": "192.168.56.101:6633"}}
+        self.my_msg_recv = msg_recv
         values = ["0.0.1.0:1", "12.12.4.2:2222", "255.255.255.255:65535"]
         for value in values:
             self.my_msg_recv['header']['ipp'] = value
-            self.assertEqual(self.message.validate_header(self.my_msg_recv['header']), True)
+            self.message = Message(self.my_msg_recv)
+            self.assertEqual(self.message.validate_header(), True)
+            del self.message
+        print msg_recv
 
 
 class TestMessageBody(unittest.TestCase):
 
     def setUp(self):
-        self.my_msg_recv = msg_recv
-        self.message = Message()
+        my_msg_recv = msg_recv
 
-    # Test message.body.suggested_id
+    # For default payload = 0, Test message.body.suggested_id
     def test_wrong_suggested_ids(self):
+        self.message = Message()
         values = [0, 'a', -1, "", 256]
         for value in values:
-            self.my_msg_recv['body']['suggested_id'] = value
-            self.assertEqual(self.message.validate_body(self.my_msg_recv['body']), False)
+            my_msg_recv['body']['suggested_id'] = value
+            self.assertEqual(self.message.validate_body(my_msg_recv['body']), False)
 
     def test_rigt_suggested_ids(self):
+        self.message = Message()
         values = [1, '1', 100, 255]
         for value in values:
-            self.my_msg_recv['body']['suggested_id'] = value
-            self.assertEqual(self.message.validate_body(self.my_msg_recv['body']), True)
+            my_msg_recv['body']['suggested_id'] = value
+            self.assertEqual(self.message.validate_body(my_msg_recv['body']), True)
 
-    # TODO: all other body possibilities
+    # For payload = 3, Test actions
+    def test_wrong_actions(self):
+        self.message = Message(payload=3)
+        values = [0, 'a', -1, "", 256]
+        for value in values:
+            my_msg_recv['body']['action'] = value
+            self.message.body.action = value
+            self.assertEqual(self.message.validate_body(my_msg_recv['body']), False)
+
+    def test_right_actions(self):
+        self.message = Message(payload=3)
+        values = ['switch_config', 'error', 'msg_received', 'entry_removed',
+                  'add_entry', 'get_statistics', 'send_probe']
+        for value in values:
+            self.message.body.action = value
+            my_msg_recv['body']['action'] = value
+            self.assertEqual(self.message.validate_body(my_msg_recv['body']), True)
 
 
 class TestFullMessage(unittest.TestCase):
@@ -137,16 +177,66 @@ class TestFullMessage(unittest.TestCase):
 
     def test_wrong_messages(self):
         for value in self.err_msgs:
-            self.my_msg_recv = value
-            self.assertEqual(self.message.validate_header(self.my_msg_recv['header']), True)
-            self.assertEqual(self.message.validate_body(self.my_msg_recv['body']), True)
+            my_msg_recv = value
+            self.assertEqual(self.message.validate_header(my_msg_recv['header']), True)
+            self.assertEqual(self.message.validate_body(my_msg_recv['body']), True)
             self.assertEqual(self.message.validate_protocol(), False)
 
     def test_right_messages(self):
         for value in self.ok_msgs:
-            self.my_msg_recv = value
-            self.assertEqual(self.message.validate_header(self.my_msg_recv['header']), True)
-            self.assertEqual(self.message.validate_body(self.my_msg_recv['body']), True)
+            my_msg_recv = value
+            self.assertEqual(self.message.validate_header(my_msg_recv['header']), True)
+            self.assertEqual(self.message.validate_body(my_msg_recv['body']), True)
+            self.assertEqual(self.message.validate_protocol(), True)
+
+
+class TestFullMessagePayloadUpdate(unittest.TestCase):
+
+    def setUp(self):
+        self.message = Message(payload=3)
+        self.err_msgs = []
+        self.ok_msgs = []
+
+        ipp = "1.1.1.1:1"
+        header = {"version": 1, "id": 1, "payload": 3, "timing": 1, "ipp": ipp}
+        body = dict()
+
+        body['action'] = 'a'
+        self.err_msgs.append({"header": header, "body": body})
+        body['action'] = 'switch'
+        self.err_msgs.append({"header": header, "body": body})
+        body['action'] = ''
+        self.err_msgs.append({"header": header, "body": body})
+        body['action'] = 0
+        self.err_msgs.append({"header": header, "body": body})
+
+        body['action'] = 'switch_config'
+        self.ok_msgs.append({"header": header, "body": body})
+        body['action'] = 'error'
+        self.ok_msgs.append({"header": header, "body": body})
+        body['action'] = 'msg_received'
+        self.ok_msgs.append({"header": header, "body": body})
+        body['action'] = 'entry_removed'
+        self.ok_msgs.append({"header": header, "body": body})
+        body['action'] = 'add_entry'
+        self.ok_msgs.append({"header": header, "body": body})
+        body['action'] = 'get_statistics'
+        self.ok_msgs.append({"header": header, "body": body})
+        body['action'] = 'send_probe'
+        self.ok_msgs.append({"header": header, "body": body})
+
+    def test_wrong_messages(self):
+        for value in self.err_msgs:
+            my_msg_recv = value
+            self.assertEqual(self.message.validate_header(my_msg_recv['header']), True)
+            self.assertEqual(self.message.validate_body(my_msg_recv['body']), True)
+            self.assertEqual(self.message.validate_protocol(), False)
+
+    def test_right_messages(self):
+        for value in self.ok_msgs:
+            my_msg_recv = value
+            self.assertEqual(self.message.validate_header(my_msg_recv['header']), True)
+            self.assertEqual(self.message.validate_body(my_msg_recv['body']), True)
             self.assertEqual(self.message.validate_protocol(), True)
 
 
