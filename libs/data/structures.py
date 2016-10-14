18b08d0be3855a8adfd886770f15c02b5c86c325
antonio <ajoaoff@gmail.com>
2016-10-13 22:00:09 -0300

    Missing files from previous commit

---
+++ libs/data/structures.py
@@ -0,0 +1,191 @@
+import collections.abc
+
+
+class NodeAttributeError(Exception):
+    pass
+
+
+class PortAttributeError(Exception):
+    pass
+
+
+def pos_int_or_none(value):
+    if value is None:
+        return (True, None)
+    try:
+        v = int(value)
+        return (True, v) if v >= 0 else (False, None)
+    except ValueError:
+        return (False, None)
+
+
+class Node(object):
+    """
+    Class to represent a Node (an openflow switch). It stores all data concerning th switch
+    """
+    def __init__(self, node):
+        self._dpid = None
+        self._controller_id = None
+        self._capabilities = None
+        self._n_tables = None
+        self._ports = list()
+        self._old_color = None
+        self._color = None
+        self._instantiate_vars(node)
+
+    def _instantiate_vars(self, node):
+        self.dpid = node.get('dpid')
+        self.controller_id = node.get('controller_id')
+        self.capabilities = node.get('capabilities')
+        self.n_tables = node.get('n_tables')
+
+    @property
+    def dpid(self):
+        return self._dpid
+
+    @dpid.setter
+    def dpid(self, my_dpid):
+        valid = True
+        try:
+            int(my_dpid, 16)
+            if len(my_dpid) != 16:
+                valid = False
+        except ValueError:
+            valid = False
+        if valid:
+            self._dpid = my_dpid
+        else:
+            raise NodeAttributeError('Datapath ID (dpid) must be a string representing a 64-bit hexadecimal')
+
+    @property
+    def controller_id(self):
+        return self._controller_id
+
+    @controller_id.setter
+    def controller_id(self, my_controller_id):
+        valid = True
+        try:
+            my_controller_id = int(my_controller_id)
+            if my_controller_id < 1 or my_controller_id > 254:
+                valid = False
+        except ValueError:
+            valid = False
+        if valid:
+            self._controller_id = int(my_controller_id)
+        else:
+            raise NodeAttributeError('Controller ID must be an integer between 1 and 254 (inclusive)')
+
+    @property
+    def capabilities(self):
+        return self._capabilities
+
+    @capabilities.setter
+    def capabilities(self, my_capabilities):
+        self._capabilities = my_capabilities
+
+    @property
+    def n_tables(self):
+        return self._n_tables
+
+    @n_tables.setter
+    def n_tables(self, my_n_tables):
+        cond, my_n_tables = pos_int_or_none(my_n_tables)
+        if cond:
+            self._n_tables = my_n_tables
+        else:
+            raise NodeAttributeError('Number of tables must be a positive integer')
+
+    @property
+    def ports(self):
+        return self._ports
+
+    @ports.setter
+    def ports(self, my_ports):
+        valid = True
+        if my_ports is not None:
+            if isinstance(my_ports, collections.abc.Sequence):
+                if not all(isinstance(p, Port) for p in my_ports):
+                    valid = False
+            else:
+                valid = False
+        else:
+            my_ports = list()
+        if valid:
+            self._ports = my_ports
+        else:
+            raise NodeAttributeError('Ports must be a list of Port instances or None')
+
+    @property
+    def old_color(self):
+        return self._old_color
+
+    @old_color.setter
+    def old_color(self, my_old_color):
+        cond, my_old_color = pos_int_or_none(my_old_color)
+        if cond:
+            self._old_color = my_old_color
+        else:
+            raise NodeAttributeError('Color must be a positive integer')
+
+    @property
+    def color(self):
+        return self._color
+
+    @color.setter
+    def color(self, my_color):
+        cond, my_color = pos_int_or_none(my_color)
+        if cond:
+            self._color = my_color
+        else:
+            raise NodeAttributeError('Color must be a positive integer')
+
+    def __repr__(self):
+        return '<Node {}, controlled by controller {}>'.format(self.dpid, self.controller_id)
+
+
+class Port(object):
+    """
+    Class to represent a port in a Node (switch)
+    """
+    def __init__(self, port):
+        self._port_no = None
+        self._name = None
+        self._speed = None
+        self._neighbors = list()
+        self._uptime = None
+
+        self._instantiate_port(port)
+
+    def _instantiate_port(self, port):
+        self.port_no = port.get('port_no')
+        self.name = port.get('name')
+
+    @property
+    def port_no(self):
+        return self._port_no
+
+    @port_no.setter
+    def port_no(self, my_port_no):
+        valid = True
+        try:
+            my_port_no = int(my_port_no)
+            if my_port_no < 0:
+                valid = False
+        except (ValueError, TypeError):
+            valid = False
+        if valid:
+            self._port_no = my_port_no
+        else:
+            raise PortAttributeError('Port number must be a positive integer')
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, my_name):
+        if isinstance(my_name, str):
+            self._name = my_name
+        else:
+            raise PortAttributeError('Name must be a string')
+
--- tests/test_structures.py
+++ tests/test_structures.py
@@ -1,64 +1,125 @@
-import unittest, threading
-from libs.signals.signals import Signal, called_on
+import unittest
+from libs.data.structures import Node, Port, NodeAttributeError, PortAttributeError
 
 
-packet_in = Signal()
-port_status = Signal()
-list_pkin1 = []
-list_pkin2 = []
-list_pstatus1 = []
-list_thread = []
-
-
-@called_on(packet_in)
-def pkin1(pkt):
-    list_pkin1.append(pkt)
-
-
-@called_on(packet_in, 'one')
-def pkin2(pkt):
-    list_pkin2.append(pkt)
-
-
-@called_on(port_status)
-def pstatus1(pkt):
-    list_pstatus1.append(pkt)
-
-
-class TestSignals(unittest.TestCase):
+class TestNodeStructure(unittest.TestCase):
 
     def setUp(self):
-        global list_pkin1, list_pkin2, list_pstatus1, list_thread
-        list_pkin1 = []
-        list_pkin2 = []
-        list_pstatus1 = []
-        list_thread = []
+        node_dict = {'dpid':'09a2c3d5e6f70912', 'controller_id':5, 'n_tables':2}
+        self.node = Node(node_dict)
 
-    def test_signals(self):
-        packet_in.send(pkt='First packet')
-        packet_in.send(sender='one', pkt='Second packet')
-        port_status.send(pkt='Third packet')
-        packet_in.send(pkt='Fourth packet')
-        port_status.send(sender='two', pkt='Fifth packet')
+    def test_dpid(self):
+        self.assertEqual(self.node.dpid, '09a2c3d5e6f70912')
+        with self.assertRaisesRegex(NodeAttributeError,
+                                    'Datapath ID \(dpid\) must be a string representing a 64-bit hexadecimal'):
+            self.node.dpid = '1234'
+        self.assertEqual(self.node.dpid, '09a2c3d5e6f70912')
+        with self.assertRaisesRegex(NodeAttributeError,
+                                    'Datapath ID \(dpid\) must be a string representing a 64-bit hexadecimal'):
+            self.node.dpid = '1234567890abcdeg'
+        self.assertEqual(self.node.dpid, '09a2c3d5e6f70912')
+        self.node.dpid = '1234567890abcdef'
+        self.assertEqual(self.node.dpid, '1234567890abcdef')
 
-        self.assertEqual(list_pkin1, ['First packet', 'Second packet', 'Fourth packet'])
-        self.assertEqual(list_pkin2, ['Second packet'])
-        self.assertEqual(list_pstatus1, ['Third packet', 'Fifth packet'])
+    def test_controller_id(self):
+        self.assertEqual(self.node.controller_id, 5)
+        self.node.controller_id = 7
+        self.assertEqual(self.node.controller_id, 7)
+        with self.assertRaisesRegex(NodeAttributeError, 'Controller ID must be an integer between 1 and 254'):
+            self.node.controller_id = 'b'
+        self.assertEqual(self.node.controller_id, 7)
+        self.node.controller_id = '98'
+        self.assertEqual(self.node.controller_id, 98)
+        with self.assertRaisesRegex(NodeAttributeError, 'Controller ID must be an integer between 1 and 254'):
+            self.node.controller_id = 278
+        self.assertEqual(self.node.controller_id, 98)
+        with self.assertRaisesRegex(NodeAttributeError, 'Controller ID must be an integer between 1 and 254'):
+            self.node.controller_id = -5
+        self.assertEqual(self.node.controller_id, 98)
 
-    def test_threads(self):
+    def test_capabilities(self):
+        self.assertEqual(self.node.capabilities, None)
+        self.node.capabilities = '123'
+        self.assertEqual(self.node.capabilities, '123')
+        self.node.capabilities = 4567
+        self.assertEqual(self.node.capabilities, 4567)
 
-        self.assertEqual(len(list_pkin1), 0)
+    def test_n_tables(self):
+        self.assertEqual(self.node.n_tables, 2)
+        self.node.n_tables = '2134'
+        self.assertEqual(self.node.n_tables, 2134)
+        with self.assertRaisesRegex(NodeAttributeError, 'Number of tables must be a positive integer'):
+            self.node.n_tables = 'fde12'
+        self.assertEqual(self.node.n_tables, 2134)
+        with self.assertRaisesRegex(NodeAttributeError, 'Number of tables must be a positive integer'):
+            self.node.n_tables = -45
+        self.assertEqual(self.node.n_tables, 2134)
+        self.node.n_tables = 65
+        self.assertEqual(self.node.n_tables, 65)
 
-        def thread_signal():
-            packet_in.send(pkt='Thread sent 1')
-            packet_in.send(sender='one', pkt='Thread sent 2')
+    def test_ports(self):
+        self.assertEqual(self.node.ports, list())
+        ports = [Port({'port_no': 6, 'name': 'P1', 'speed': 100}),
+                 Port({'port_no': 5, 'name': 'P2', 'speed': 1000})]
+        self.node.ports = ports
+        self.assertEqual(self.node.ports, ports)
+        self.node.ports = None
+        self.assertEqual(self.node.ports, list())
+        with self.assertRaisesRegex(NodeAttributeError, 'Ports must be a list of Port instances or None'):
+            self.node.ports = ['a', Port({'port_no': 6, 'name': 'P6', 'speed': 10000})]
+        self.assertEqual(self.node.ports, list())
+        with self.assertRaisesRegex(NodeAttributeError, 'Ports must be a list of Port instances or None'):
+            self.node.ports = Port({'port_no': 8, 'name': 'P7', 'speed': 1000})
+        self.assertEqual(self.node.ports, list())
 
-        packet_in.send(pkt='Main sent 1')
-        thread = threading.Thread(target=thread_signal)
-        thread.start()
+    def colors_testing(self, fieldname):
+        self.assertEqual(getattr(self.node, fieldname), None)
+        setattr(self.node, fieldname, 543)
+        self.assertEqual(getattr(self.node, fieldname), 543)
+        with self.assertRaisesRegex(NodeAttributeError, 'Color must be a positive integer'):
+            setattr(self.node, fieldname, 'adde')
+        self.assertEqual(getattr(self.node, fieldname), 543)
+        setattr(self.node, fieldname, None)
+        self.assertEqual(getattr(self.node, fieldname), None)
 
-        self.assertEqual(list_pkin1, ['Main sent 1', 'Thread sent 1', 'Thread sent 2'])
-        self.assertEqual(list_pkin2, ['Thread sent 2'])
+    def test_old_color(self):
+        self.colors_testing('old_color')
+
+    def test_color(self):
+        self.colors_testing('color')
+
+
+class TestPortStructure(unittest.TestCase):
+
+    def setUp(self):
+        port_dict = {'port_no': 1, 'name':'GigabitEthernet1/1', 'speed':1000000000}
+        self.port = Port(port_dict)
+
+    def test_port_no(self):
+        self.assertEqual(self.port.port_no, 1)
+        self.port.port_no = '43'
+        self.assertEqual(self.port.port_no, 43)
+        with self.assertRaisesRegex(PortAttributeError, 'Port number must be a positive integer'):
+            self.port.port_no = -23
+        self.assertEqual(self.port.port_no, 43)
+        with self.assertRaisesRegex(PortAttributeError, 'Port number must be a positive integer'):
+            self.port.port_no = 'abc'
+        self.assertEqual(self.port.port_no, 43)
+        with self.assertRaisesRegex(PortAttributeError, 'Port number must be a positive integer'):
+            self.port.port_no = None
+        self.assertEqual(self.port.port_no, 43)
+
+    def test_name(self):
+        self.assertEqual(self.port.name, 'GigabitEthernet1/1')
+        self.port.name = 'abcdefg'
+        self.assertEqual(self.port.name, 'abcdefg')
+        with self.assertRaisesRegex(PortAttributeError, 'Name must be a string'):
+            self.port.name = []
+        self.assertEqual(self.port.name, 'abcdefg')
+        with self.assertRaisesRegex(PortAttributeError, 'Name must be a string'):
+            self.port.name = None
+        self.assertEqual(self.port.name, 'abcdefg')
+
 
 
 if __name__ == '__main__':
