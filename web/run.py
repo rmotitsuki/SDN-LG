05920613b277f77aa4e13f9f0f7bb613be2b2ad6
Rogerio Motitsuki <rogerio.motitsuki@gmail.com>
2016-10-17 17:53:16 -0200

    how to run, comments.

--- web/run.py
+++ web/run.py
@@ -1,4 +1,14 @@
 from sdnlg_website import app
 
+"""
+Use of Flask, a lightweight Python web framework.
+
+Install:
+    pip install Flask
+Run:
+    python run.py
+"""
+
+
 if __name__ == '__main__':
     app.run()
