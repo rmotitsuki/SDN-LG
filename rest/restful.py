8d0e6283f1ff59d2ceec46f9cbb8d49116532d6e
antonio <ajoaoff@gmail.com>
2016-11-08 11:06:39 -0200

    Bug corrected. REST calls should return the object, not JSON, as it is converted by flask_restful library.

--- rest/restful.py
+++ rest/restful.py
@@ -16,7 +16,7 @@ class Switches(Resource):
         for s in core.switches:
             returns.append({'dpid': s.dpid, 'capabilities': s.capabilities, 'n_tables': s.n_tables,
                             'n_ports': len(s.ports)})
-        return json.dumps(returns)
+        return returns
 
 
 # Returns the list of ports from a switch
@@ -29,7 +29,7 @@ class Ports(Resource):
             return json.dumps([])
         for p in s.ports:
             returns.append({'port_no': p.port_no, 'name': p.name, 'speed': p.speed, 'uptime': p.uptime})
-        return json.dumps(returns)
+        return returns
 
 
 # Returns the list of links
@@ -40,8 +40,9 @@ class Links(Resource):
         for n1,p1,n2,p2,speed in core.links:
             returns.append({'node1': {'dpid': n1.dpid, 'port': {'port_no': p1.port_no, 'name': p1.name}},
                             'node2': {'dpid': n2.dpid, 'port': {'port_no': p2.port_no, 'name': p2.name}},
-                            'speed': speed })
+                            'speed': speed})
         return returns
+
 
 api.add_resource(Switches, '/switches')
 api.add_resource(Ports, '/switches/<dpid>/ports')
